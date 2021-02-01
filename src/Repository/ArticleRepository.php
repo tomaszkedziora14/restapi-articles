69a82a7ba8882fccb3fac4649cf36ce125346024
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2021-02-01 14:47:27 +0100

    Update ArticleRepository.php

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -16,7 +16,7 @@ class ArticleRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, User::class);
+        parent::__construct($registry, Article::class);
     }
 
     /**
